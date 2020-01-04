d486e64290d90188c615e5cb8016123af0c647c9
Versteeg0 <49746367+Versteeg0@users.noreply.github.com>
2020-01-04 22:25:49 +0100

    Duck, Monday&Tuesday discount done.

--- BeestjeOpJeFeestje/Controllers/HomeController.cs
+++ BeestjeOpJeFeestje/Controllers/HomeController.cs
@@ -153,11 +153,28 @@ namespace BeestjeOpJeFeestje.Controllers
         private decimal CalculateTotalPrice(BoekingVM boekingVM)
         {
             decimal totalprice = 0;
+            List<string> kortinglijst = new List<string>();
 
-            foreach(Beestje b in boekingVM.SelectedBeestjes)
-                totalprice += b.Price;
+            foreach (Beestje b in boekingVM.SelectedBeestjes)
+            {
+                if (b.Name == "Eend")
+                {
+                    Random r = new Random();
+                    int korting = r.Next(0, 7);
 
-            foreach(Accessoires a in boekingVM.SelectedAccessoires)
+                    if (korting == 1)
+                    {
+                        totalprice = b.Price / 2;
+                        kortinglijst.Add("Eend 50%");
+                    }
+
+                }
+                DayOfWeek day = boekingVM.Date.DayOfWeek;
+                if (day == DayOfWeek.Monday || day == DayOfWeek.Tuesday)
+                    totalprice = b.Price - ((b.Price / 100) * 15);
+            }
+
+            foreach (Accessoires a in boekingVM.SelectedAccessoires)
                 totalprice += a.Price;
 
             return totalprice;
