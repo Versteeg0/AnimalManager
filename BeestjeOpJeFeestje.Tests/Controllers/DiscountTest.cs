c5bc363076c7a3d245c8b2802217c23a8113c510
Versteeg0 <49746367+Versteeg0@users.noreply.github.com>
2020-01-07 15:34:17 +0100

    Discount test ThreeOfAKind

--- BeestjeOpJeFeestje.Tests/Controllers/DiscountTest.cs
+++ BeestjeOpJeFeestje.Tests/Controllers/DiscountTest.cs
@@ -1,14 +1,96 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using BeestjeOpJeFeestje.Controllers;
+using BeestjeOpJeFeestje.Discount;
+using BeestjeOpJeFeestje.Models;
+using BeestjeOpJeFeestje.Repos;
+using BeestjeOpJeFeestje.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BeestjeOpJeFeestje.Tests.Controllers
 {
     [TestClass]
     public class DiscountTest
     {
+        [TestMethod]
+        public void ThreeOfAKind()
+        {
+            // Arrange
+            Mock<BoekingVM> boekingVM = new Mock<BoekingVM>();
+
+            
+            Mock<Beestje> animal1 = new Mock<Beestje>();
+            Mock<Beestje> animal2 = new Mock<Beestje>();
+            Mock<Beestje> animal3 = new Mock<Beestje>();
+
+            // If these are not initialized correctly, automatically takes the 2% discount
+            animal1.Object.Name = "b";
+            animal2.Object.Name = "c";
+            animal3.Object.Name = "d";
+
+            animal1.Object.Price = 10.00m;
+            animal2.Object.Price = 10.00m;
+            animal3.Object.Price = 10.00m;
+
+            animal1.Object.Type = "Boerderij";
+            animal2.Object.Type = "Boerderij";
+            animal3.Object.Type = "Boerderij";
+
+            // If no date is selected, takes date of today which could be Monday/Tuesday
+            boekingVM.Object.Date = new DateTime(2020, 1, 8);
+
+            boekingVM.Object.SelectedBeestjes.Add(animal1.Object);
+            boekingVM.Object.SelectedBeestjes.Add(animal2.Object);
+            boekingVM.Object.SelectedBeestjes.Add(animal3.Object);
+
+            CalculateDiscount discount = new CalculateDiscount();
+
+            // Act
+            var result = discount.CalculateTotalPrice(boekingVM.Object);
+
+            // Assert
+            Assert.AreEqual(27.00m, result);
+        }
+
+        [TestMethod]
+        public void ThreeOfAKind()
+        {
+            // Arrange
+            Mock<BoekingVM> boekingVM = new Mock<BoekingVM>();
+
+
+            Mock<Beestje> animal1 = new Mock<Beestje>();
+            Mock<Beestje> animal2 = new Mock<Beestje>();
+            Mock<Beestje> animal3 = new Mock<Beestje>();
+
+            // If these are not initialized correctly, automatically takes the 2% discount
+            animal1.Object.Name = "b";
+            animal2.Object.Name = "c";
+            animal3.Object.Name = "d";
+
+            animal1.Object.Price = 10.00m;
+            animal2.Object.Price = 10.00m;
+            animal3.Object.Price = 10.00m;
+
+            animal1.Object.Type = "Boerderij";
+            animal2.Object.Type = "Boerderij";
+            animal3.Object.Type = "Boerderij";
+
+            // If no date is selected, takes date of today which could be Monday/Tuesday
+            boekingVM.Object.Date = new DateTime(2020, 1, 8);
+
+            boekingVM.Object.SelectedBeestjes.Add(animal1.Object);
+            boekingVM.Object.SelectedBeestjes.Add(animal2.Object);
+            boekingVM.Object.SelectedBeestjes.Add(animal3.Object);
+
+            CalculateDiscount discount = new CalculateDiscount();
+
+            // Act
+            var result = discount.CalculateTotalPrice(boekingVM.Object);
+
+            // Assert
+            Assert.AreEqual(27.00m, result);
+        }
+
     }
 }
